6f681e71ae80e60535f7e4817d13b383fc0c911c
木易 <oncwnuKMS2M73vNWCP6rN8E61Lh0@git.weixin.qq.com>
2020-03-12 21:18:19 +0800

    增加ffmpeg操作文件

---
+++ VideoDeal/VideoDeal/Helper/ffmpegHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoDeal.Helper
+{
+    public class ffmpegHelper
+    {
+        /// <summary>
+        /// 获取ffmpeg.exe文件路径
+        /// </summary>
+        public static string FFmpegPath = Environment.CurrentDirectory + "\\ffmpeg\\ffmpeg.exe";
+
+    }
+}
--- VideoDeal/VideoDeal/MainWindow.xaml.cs
+++ VideoDeal/VideoDeal/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VideoDeal.Helper;
 
 namespace VideoDeal
 {
@@ -23,6 +24,7 @@ namespace VideoDeal
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show(ffmpegHelper.FFmpegPath);
         }
     }
 }
