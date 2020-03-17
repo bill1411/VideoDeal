32341b09151a9ebd88e89a9823cdfb711028597a
15989077170 <346278334@qq.com>
2020-03-17 15:51:51 +0800

    增加方法及界面布局

--- VideoDeal/VideoDeal/MainWindow.xaml.cs
+++ VideoDeal/VideoDeal/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,15 +28,33 @@ namespace VideoDeal
         /// 定义原视频文件路径
         /// </summary>
         private static string videoPath = string.Empty;
+        /// <summary>
+        /// 文件过滤条件
+        /// </summary>
+        private static readonly string fileFilter = "MP4文件|*.mp4|flv文件|*.flv|所有文件|*.*";
+        /// <summary>
+        /// 视频过滤条件
+        /// </summary>
+        private static readonly string videoFilter = "MP4文件|*.mp4|flv文件|*.flv";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            #region 加载图片
+            BitmapImage bt = new BitmapImage(new Uri("Img\\icon\\copy.png", UriKind.Relative));
+            this.copyVideoId.Source = bt;
+            this.copyVideoAuth.Source = bt;
+            this.copyVideoAddress.Source = bt;
+            this.copySql.Source = bt;
+            #endregion
+
         }
 
         #region 打开视频
         private void btnAddVideo_Click(object sender, RoutedEventArgs e)
         {
-            videoPath = OpenFileDialog("MP4文件|*.mp4|flv文件|*.flv|所有文件|*.*");
+            videoPath = OpenFileDialog(fileFilter);
         }
         #endregion
 
@@ -43,7 +63,7 @@ namespace VideoDeal
         {
             if (!string.IsNullOrWhiteSpace(videoPath))
             {
-                string dealVideFilePath = SaveFileDialog("MP4文件|*.mp4|flv文件|*.flv");
+                string dealVideFilePath = SaveFileDialog(videoFilter);
                 bool result = ffmpegHelper.VideoRotate(videoPath, dealVideFilePath, "2");
                 if (result)
                     MessageBox.Show("操作成功");
@@ -60,7 +80,7 @@ namespace VideoDeal
         {
             if (!string.IsNullOrWhiteSpace(videoPath))
             {
-                string dealVideFilePath = SaveFileDialog("MP4文件|*.mp4|flv文件|*.flv");
+                string dealVideFilePath = SaveFileDialog(videoFilter);
                 bool result = ffmpegHelper.VideoRotate(videoPath, dealVideFilePath, "1");
                 if (result)
                     MessageBox.Show("操作成功");
@@ -85,9 +105,29 @@ namespace VideoDeal
         }
         #endregion
 
-        #region 上传文件到阿里云
+        #region 获取视频上传参数【auth、address及Id】
+        /// <summary>
+        /// 获取视频上传参数【auth、address及Id】
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnUpload_Click(object sender, RoutedEventArgs e)
         {
+            #region 清空内容
+            this.txtVideoAddress.Text = "";
+            this.txtVideoAuth.Text = "";
+            this.txtVideoId.Text = "";
+            #endregion
+
+            //获取文件路径
+            string videoPath = OpenFileDialog(fileFilter);
+            //获取文件名
+            string title = System.IO.Path.GetFileNameWithoutExtension(videoPath);
+            //获取上传视频的参数
+            ALiYunHelper.GetVideoUploadParameter(title,videoPath);
+            txtVideoId.Text = ALiYunHelper.videoId;
+            txtVideoAuth.Text = ALiYunHelper.videoToken;
+            txtVideoAddress.Text = ALiYunHelper.videoAddress;
 
         }
         #endregion
@@ -105,19 +145,73 @@ namespace VideoDeal
         }
         #endregion
 
-        #region 截图
+        #region 获取图片及视频参数并生成Sql语句
         private void btnCutImg_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(videoPath))
+            if (string.IsNullOrWhiteSpace(ALiYunHelper.videoAddress))
             {
-                MessageBox.Show("未添加源视频", "系统提示：", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("参数不完整", "系统提示：", MessageBoxButton.OK, MessageBoxImage.Information);
                 return;
             }
-
-
+            //调用获取url的方法
+            ALiYunHelper.GetPlayUrl(ALiYunHelper.videoId);
+            if (!string.IsNullOrWhiteSpace(ALiYunHelper.videoImgUrl) && !string.IsNullOrWhiteSpace(ALiYunHelper.videoUrl))
+            {
+                this.txtSql.Text = "insert into be_video values('" + ALiYunHelper.videoId + "',"
+                                                             + "'" + ALiYunHelper.videoImgUrl + "',"
+                                                             + "'" + txtCustomerName.Text + "押车视频',"
+                                                             + "1,getdate(),getdate(),1,0,"
+                                                             + "'" + ALiYunHelper.videoUrl + "')";
+            }
+            else
+            {
+                MessageBox.Show("参数不完整，请排查操作过程");
+            }
 
         }
         #endregion
+
+        #region 打开上传页面
+        private void btnOpenHtml_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://api.clxchina.cn:9099/aliyun_video_upload/video-upload.html");
+        }
+        #endregion
+
+        #region 拷贝上传地址
+        private void clikc_copy_void_address(object sender, RoutedEventArgs e)
+        { 
+            Clipboard.SetText(this.txtVideoAddress.Text);
+            MessageBox.Show("数据复制成功");
+
+        }
+        #endregion
+
+        #region 拷贝上传凭证
+        private void clikc_copy_void_auth(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(this.txtVideoAuth.Text);
+            MessageBox.Show("数据复制成功");
+        }
+        #endregion
+
+        #region 拷贝上传视频ID
+        private void clikc_copy_void_id(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(this.txtVideoId.Text);
+            MessageBox.Show("数据复制成功");
+        }
+        #endregion
+
+        #region 拷贝最终生成的sql语句
+        private void clikc_copy_sql(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(this.txtSql.Text);
+            MessageBox.Show("数据复制成功");
+        }
+        #endregion
+
+
 
         #region 打开文件对话框
         /// <summary>
@@ -162,5 +256,6 @@ namespace VideoDeal
                 return "" ;
         }
         #endregion
+
     }
 }
